bf152b7579f8bec4fd10962aad792b1a39cf94c0
cavanosa <cavanosa@cavanosa.cav>
2021-07-30 22:44:10 +0200

    first commit

---
+++ src/main/java/com/tutorial/tutorialemail/TutorialEmailApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.tutorialemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/tutorialemail/TutorialEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.tutorialemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
